7b5bf28720210bc8c0bdc25f320dd6b9ac9feaef
alex <alex75210903@gmail.com>
2024-05-28 19:29:30 -0500

    pokemon

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
 console.log("peticiones")
 
-const obterpokemon = async () => 
-    const respuesta = await fetch ("https://pokeapi.co/api/v2/pokemon/pikachu")
+const obtener = () => {
+    console.log("obtener")
+}
+obtener()
+
+const buttonobtener = document.getElementById("button-obtener")
+
+//buttonobtener.addEventListener("click",obtener)
+buttonobtener.addEventListener("click",async() =>{
+    console.log("obtener")
+    const pokemondata = await obterPokemon()
+    const h3 = document.createElement("h3")
+    h3.innerText= pokemondata.name
+
+    pokemonarticle.appendChild(h3)
+
+    const img =document.createElement("img")
+    img.src = pokemondata.sprites.front_defaultpokemonarticle.appendChild(img)
+
+    const pokemonarticle = document.getElementById("pokemon")
+})
+
+
+const obterPokemon = async () => {
+    const respuesta =await fetch ("https://pokeapi.co/api/v2/pokemon/pikachu")
     const data = await respuesta.json ()
     console.log(data)
+}
 
     obterpokemon ()
